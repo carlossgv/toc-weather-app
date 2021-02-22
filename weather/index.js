762cf5576f7eadffea073d15c68f386548af5668
Carlos Gonzalez <carlossgv@gmail.com>
2021-02-22 15:43:32 -0300

    project finished but ugly

--- weather/index.js
+++ weather/index.js
@@ -1,6 +1,35 @@
-import testFunc from './utils';
+import getWeather from './utils';
 
-testFunc('Carlos Santiago Gonzalez');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
+loader.style.display = 'none';
+
+form.addEventListener('submit', function (e) {
+  main.style.display = 'none';
+  loader.style.display = 'block';
+  getWeather(input.value).then((weather) => {
+    loader.style.display = 'none';
+    for (let prop in weather) {
+      let div = document.querySelector(`.${prop}_div`);
+      div.innerHTML = '';
+
+      let h3 = document.createElement('h3');
+      h3.innerHTML = `${weather[prop]}`;
+
+      div.append(h3);
+      main.append(div);
+
+      console.log(prop, weather[prop]);
+    }
+    main.style.display = 'block';
+  });
+
+  input.value = '';
+
+  e.preventDefault();
+});
 
 if (module.hot) {
   module.hot.accept();
--- weather/utils.js
+++ weather/utils.js
@@ -1,5 +1,29 @@
-function testFunc(name) {
-  console.log(name);
+const fetchWeather = async (city) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=cc2320d0a9fdacdd2495b7390e523460`
+  );
+  const weatherInfo = await response.json();
+
+  return weatherInfo;
+};
+
+const getWeather = async (city) => {
+  let weatherInfo = await fetchWeather(city);
+  const weather = new Weather(weatherInfo);
+
+  return weather;
+};
+
+class Weather {
+  constructor(weatherInfo) {
+    this.name = weatherInfo.name;
+    this.temp = `Temp: ${weatherInfo.main.temp}°C`;
+    this.temp_max = `Max. Temp: ${weatherInfo.main.temp_max}°C`;
+    this.temp_min = `Min. Temp: ${weatherInfo.main.temp_min}°C`;
+    this.humidity = `Humidity: ${weatherInfo.main.humidity}%`;
+    this.weather_main = weatherInfo.weather[0].main;
+    // this.weather_icon = weatherInfo.weather[0].icon;
+  }
 }
 
-export default testFunc;
+export default getWeather;
